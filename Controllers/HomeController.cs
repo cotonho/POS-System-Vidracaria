06f42ae89963acd2a804f9caf578e1cc9a601a3c
Cotonho <marcoantoniomilagre587@gmail.com>
2025-09-04 14:39:37 -0300

    Mudança na exibição das paginas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,23 +17,7 @@ namespace VidracariaDoMarcinho.Controllers
 
         public IActionResult Index()
         {
-            var Table_Clientes = new DataTable();
-            Table_Clientes.Columns.Add("CPF", typeof(string));
-            Table_Clientes.Columns.Add("Nome", typeof(string));
-            Table_Clientes.Columns.Add("Telefone", typeof(string));
-
-            Table_Clientes.Rows.Add("12345", "João", "(32)984000000");
-            Table_Clientes.Rows.Add("67890", "Bão", "(32)098765432");
-            Table_Clientes.Rows.Add("10112", "Leão", "(32)123455789");
-
-            //filtro
-            //DataRow[] results = Table_Clientes.Select("CPF > 10112");
-            //Table_Clientes = results.CopyToDataTable();
-
-            Table_Clientes.DefaultView.Sort = "Nome DESC";
-
-            Table_Clientes = Table_Clientes.DefaultView.ToTable();
-            return View(Table_Clientes);
+            return View();
         }
 
         public IActionResult Privacy()
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,17 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-const abrir = document.getElementById('botao_clientes');
-const fechar = document.getElementById('fechar');
-const painel = document.getElementById('painel');
+//const abrir = document.getElementById('botao_clientes');
+//const fechar = document.getElementById('fechar');
+//const painel = document.getElementById('painel');
 
-abrir.addEventListener('click', () => {
-    painel.classList.add('ativo');
-});
+//abrir.addEventListener('click', () => {
+//    painel.classList.add('ativo');
+//});
 
-fechar.addEventListener('click', () => {
-    painel.classList.remove('ativo');
-});
+//fechar.addEventListener('click', () => {
+//    painel.classList.remove('ativo');
+//});
 
 $(document).ready(function () {
     $('.table').DataTable();
