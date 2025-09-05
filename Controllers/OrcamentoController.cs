c3d00047270b15228967614ba4e1417b97bf4ffd
Cotonho <marcoantoniomilagre587@gmail.com>
2025-09-05 14:50:18 -0300

    Adicionada a pagina de orçamento

---
+++ Controllers/OrcamentoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VidracariaDoMarcinho.Data;
+using VidracariaDoMarcinho.Models;
+
+namespace VidracariaDoMarcinho.Controllers
+{
+    public class OrcamentoController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        //public OrcamentoController(AppDbContext)
+        //{
+        //    _context = context;
+        //}
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
