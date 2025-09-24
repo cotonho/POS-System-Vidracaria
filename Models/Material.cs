d0a1383b2ad8b384deab75903960e837a817704a
Cotonho <marcoantoniomilagre587@gmail.com>
2025-09-24 14:59:41 -0300

    LGTM

--- Models/Material.cs
+++ Models/Material.cs
@@ -11,7 +11,7 @@ namespace VidracariaDoMarcinho.Models
         public int Id { get; set; }
         public string Nome { get; set; }
         public decimal Preco { get; set; }
-        public string Cor { get; set; }
+        public string? Cor { get; set; }
 
     }
 }
